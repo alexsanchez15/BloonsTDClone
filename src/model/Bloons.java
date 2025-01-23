22687417413f1bc1b3cfdcd5d17a679a103376f0
alexsanchez15 <alexefsanchez@gmail.com>
2025-01-23 12:05:02 -0500

    easier to work run changes

--- src/model/Bloons.java
+++ src/model/Bloons.java
@@ -14,109 +14,100 @@ import javafx.stage.Stage;
 
 public class Bloons extends Application {
 
-	public static final int TARGET_FPS = 60;
+        public static final int TARGET_FPS = 60;
 
-    private static final double INTERVAL = 1_000_000_000.0 / TARGET_FPS; // interval in nanoseconds
+        private static final double INTERVAL = 1_000_000_000.0 / TARGET_FPS; // interval in nanoseconds
 
-    private static GameManager gm;
+        private static GameManager gm;
 
-	
-	Pane root;
-	Scene scene;
-	Timeline timeline;
+        Pane root;
+        Scene scene;
+        Timeline timeline;
 
+        public static void main(String[] args) {
+                gm = new GameManager();
+                launch(args);
+        }
 
-	public static void main(String[] args) {
-		gm = new GameManager();
-		launch(args);
-	}
+        @Override
+        public void start(Stage primaryStage) {
 
-	@Override
-	public void start(Stage primaryStage) {
-		
-        root = new Pane();
-        
-        scene = new Scene(root);
-        
-        scene.getStylesheets().add("/model/StyledButton.css");
-        System.out.println(scene.getStylesheets());
-        
-        Canvas canvas = new Canvas(GameManager.resX, GameManager.resY);
-        root.getChildren().add(canvas);
-        
-       
-      
+                root = new Pane();
 
-       
-         root.getChildren().add(gm.getRoundButton());
-       
-        
-        primaryStage.setResizable(false);
+                scene = new Scene(root);
 
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("BloonsTDClone");
-        primaryStage.show();
-        
-        canvas.addEventHandler(MouseEvent.MOUSE_MOVED, e -> {
-        	
-        	gm.handleMouseMovement((int)e.getX(), (int)e.getY());
-        });
-        
-        canvas.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
-        	
-        	gm.handleMouseClick((int)e.getX(), (int)e.getY());
-        });
-        
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+                scene.getStylesheets().add("/sprites/layout/StyledButton.css");
+                System.out.println(scene.getStylesheets());
 
-        AnimationTimer gameLoop = new AnimationTimer() {
-            private long lastUpdate = 0;
-            private long accumulatedTime = 0;
+                Canvas canvas = new Canvas(GameManager.resX, GameManager.resY);
+                root.getChildren().add(canvas);
 
-            @Override
-            public void handle(long now) {
-                if (lastUpdate == 0) {
-                    lastUpdate = now;
-                    return;
-                }
+                root.getChildren().add(gm.getRoundButton());
 
-                long deltaTime = now - lastUpdate;
-                lastUpdate = now;
-                accumulatedTime += deltaTime;
+                primaryStage.setResizable(false);
 
-                if(!GameManager.fastFowarded) {
-                	while (accumulatedTime >= (INTERVAL)) {
-                		render(gc);
-                    	update();
-                    
-                    	accumulatedTime -= INTERVAL;
-                	}
-                }else {
-                	while (accumulatedTime >= (INTERVAL)/2) {
-                		render(gc);
-                    	update();
-                    
-                    	accumulatedTime -= INTERVAL/2;
-                	}
-                }
-                
+                primaryStage.setScene(scene);
+                primaryStage.setTitle("BloonsTDClone");
+                primaryStage.show();
 
-                
-            }
-        };
+                canvas.addEventHandler(MouseEvent.MOUSE_MOVED, e -> {
 
-        gameLoop.start();
-    }
+                        gm.handleMouseMovement((int) e.getX(), (int) e.getY());
+                });
 
-	int i = 0;
+                canvas.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
 
-	private void update() {
-		
-		gm.update();
-		
-	}
+                        gm.handleMouseClick((int) e.getX(), (int) e.getY());
+                });
 
-	private void render(GraphicsContext gc) {
-		gm.draw(gc);
-	}
+                GraphicsContext gc = canvas.getGraphicsContext2D();
+
+                AnimationTimer gameLoop = new AnimationTimer() {
+                        private long lastUpdate = 0;
+                        private long accumulatedTime = 0;
+
+                        @Override
+                        public void handle(long now) {
+                                if (lastUpdate == 0) {
+                                        lastUpdate = now;
+                                        return;
+                                }
+
+                                long deltaTime = now - lastUpdate;
+                                lastUpdate = now;
+                                accumulatedTime += deltaTime;
+
+                                if (!GameManager.fastFowarded) {
+                                        while (accumulatedTime >= (INTERVAL)) {
+                                                render(gc);
+                                                update();
+
+                                                accumulatedTime -= INTERVAL;
+                                        }
+                                } else {
+                                        while (accumulatedTime >= (INTERVAL) / 2) {
+                                                render(gc);
+                                                update();
+
+                                                accumulatedTime -= INTERVAL / 2;
+                                        }
+                                }
+
+                        }
+                };
+
+                gameLoop.start();
+        }
+
+        int i = 0;
+
+        private void update() {
+
+                gm.update();
+
+        }
+
+        private void render(GraphicsContext gc) {
+                gm.draw(gc);
+        }
 }
